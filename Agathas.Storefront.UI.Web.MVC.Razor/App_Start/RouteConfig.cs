fb2354fe1c5f90505ff02e85293afe8f3a39b6e1
ekiwsant <ekiwsant@THP7DDN86HY1>
2019-09-16 12:03:12 +0700

    update dependencies

---
+++ Agathas.Storefront.UI.Web.MVC.Razor/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Agathas.Storefront.UI.Web.MVC.Razor
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
+
+            routes.MapRoute(
+                "ProductDetail",                                                // Route name
+                "Product/{brand}-{productname}/{id}",                          // URL with parameters
+                new { controller = "Product", action = "Detail", id = "" } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "Browse",                                                // Route name
+                "Category/{category}/{categoryId}",                                  // URL with parameters
+                new { controller = "Product", action = "GetProductsByCategory", id = "" } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }  // Parameter defaults
+            );
+
+        }
+    }
+}
---
+++ Agathas.Storefront.UI.Web.MVC.Razor/Global.asax.cs
@@ -0,0 +1,48 @@
+﻿using Agathas.Storefront.Controllers;
+using Agathas.Storefront.Infrastructure.Configuration;
+using Agathas.Storefront.Infrastructure.Domain.Events;
+using Agathas.Storefront.Infrastructure.Email;
+using Agathas.Storefront.Infrastructure.Logging;
+using StructureMap;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Agathas.Storefront.UI.Web.MVC.Razor
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
+
+            BootStrapper.ConfigureDependencies();
+
+            Controllers.AutoMapperBootStrapper.ConfigureAutoMapper();
+            Services.AutoMapperBootStrapper.ConfigureAutoMapper();
+
+            ApplicationSettingsFactory.InitializeApplicationSettingsFactory(ObjectFactory.GetInstance<IApplicationSettings>());
+
+            LoggingFactory.InitializeLogFactory(ObjectFactory.GetInstance<ILogger>());
+
+            EmailServiceFactory.InitializeEmailServiceFactory(ObjectFactory.GetInstance<IEmailService>());
+
+            ControllerBuilder.Current.SetControllerFactory(new IoCControllerFactory());
+
+            DomainEvents.DomainEventHandlerFactory = ObjectFactory.GetInstance<IDomainEventHandlerFactory>();
+
+            Repository.NHibernate.SessionFactory.Init();
+            Services.Presentation.SessionFactory.Init();
+
+            LoggingFactory.GetLogger().Log("Application Started");
+
+        }
+
+    }
+}
